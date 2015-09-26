22d8c861a000b916ec4d40626e8a6c6f06ec2c94
Sai Arvind <sai.arvindg@gmail.com>
2015-09-26 14:57:53 -0400

    imageinfo updated

--- DaemonDash/src/ImageInfo.java
+++ DaemonDash/src/ImageInfo.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,13 +8,35 @@ import javax.imageio.ImageIO;
 
 public class ImageInfo {
 	
-	final int CHUNKS = 64; //number of chunks the image should be split into
 	
-	BufferedImage mImg;
 	
-	public void readImg(File imgFile) throws IOException{
+	public void getImgChunks(File imgFile) throws IOException{
 		FileInputStream mFileInputStream = new FileInputStream(imgFile);
-		mImg = ImageIO.read(mFileInputStream);
+		BufferedImage buffImg = ImageIO.read(mFileInputStream);
+		
+		int rows = 4;  
+        int cols = 4;  
+        int chunks = rows * cols; 
+		
+		int chunkHeight = buffImg.getHeight();
+		int chunkWidth = buffImg.getWidth();
+		int count = 0;
+		
+		BufferedImage imgChunks[] = new BufferedImage[chunks];
+		
+		for (int x = 0; x < rows; x++) {  
+            for (int y = 0; y < cols; y++) {  
+                //Initialize the image array with image chunks  
+                imgChunks[count] = new BufferedImage(chunkWidth, chunkHeight, buffImg.getType()); 
+                
+                Graphics2D gr = imgChunks[count++].createGraphics();  
+                gr.drawImage(buffImg, 0, 0, chunkWidth, chunkHeight, chunkWidth * y, chunkHeight * x, chunkWidth * y + chunkWidth, chunkHeight * x + chunkHeight, null);  
+                gr.dispose(); 
+            }
+		}
+		
 	}
+		
+
 
 }
