c8b2e5cded4c397a604f594d303050ca80d77c9d
Sai Arvind <sai.arvindg@gmail.com>
2015-09-25 23:45:29 -0400

    added hell github

---
+++ PracticeProject/com/sai/cmsc131/HelloGithub.java
@@ -0,0 +1,10 @@
+package com.sai.cmsc131;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github!");
+	}
+
+}
