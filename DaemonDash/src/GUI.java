b9f2e050d973271e7c58e92427ef05aa40188253
TechMatt1337 <mattgmb@yahoo.com>
2015-09-26 15:32:41 -0400

    Added 6 buttons and reformatted progress bar

--- DaemonDash/src/GUI.java
+++ DaemonDash/src/GUI.java
@@ -40,11 +40,10 @@ public class GUI extends JFrame {
         Border mouseOverBorder = new LineBorder(new Color(200,200,200),4);
         
         JPanel button1 = new JPanel();
-        button1.setSize(100, 100);
+        button1.setSize(120, 120);
         button1.setBorder(null);
         button1.setBackground(new Color(160,160,160));
-        button1.setLocation(this.getWidth()/2-button1.getWidth()/2, 
-        		this.getHeight()/2-button1.getHeight()/2-50);  
+        button1.setLocation(580,20);  
         button1.addMouseListener(new MouseListener() {
             @Override
             public void mouseReleased(MouseEvent e) {
@@ -73,13 +72,178 @@ public class GUI extends JFrame {
             }
         });
         
+        JPanel button2 = new JPanel();
+        button2.setSize(120, 120);
+        button2.setBorder(null);
+        button2.setBackground(new Color(160,160,160));
+        button2.setLocation(720,20);  
+        button2.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button2.setBackground(new Color(160,160,160));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // Not working :(
+                button2.setBackground(new Color(200,200,200));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button2.setBorder(null);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button2.setBorder(mouseOverBorder);
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            	//button2.setBackground(new Color(120,120,120));
+            }
+        });
+        
+        JPanel button3 = new JPanel();
+        button3.setSize(120, 120);
+        button3.setBorder(null);
+        button3.setBackground(new Color(160,160,160));
+        button3.setLocation(580,160);  
+        button3.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button3.setBackground(new Color(160,160,160));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // Not working :(
+                button3.setBackground(new Color(200,200,200));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button3.setBorder(null);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button3.setBorder(mouseOverBorder);
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            	//button3.setBackground(new Color(120,120,120));
+            }
+        });
+        
+        JPanel button4 = new JPanel();
+        button4.setSize(120, 120);
+        button4.setBorder(null);
+        button4.setBackground(new Color(160,160,160));
+        button4.setLocation(720,160);  
+        button4.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button4.setBackground(new Color(160,160,160));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // Not working :(
+                button4.setBackground(new Color(200,200,200));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button4.setBorder(null);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button4.setBorder(mouseOverBorder);
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            	//button4.setBackground(new Color(120,120,120));
+            }
+        });
+        
+        JPanel button5 = new JPanel();
+        button5.setSize(120, 120);
+        button5.setBorder(null);
+        button5.setBackground(new Color(160,160,160));
+        button5.setLocation(580,300);  
+        button5.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button5.setBackground(new Color(160,160,160));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // Not working :(
+                button5.setBackground(new Color(200,200,200));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button5.setBorder(null);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button5.setBorder(mouseOverBorder);
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            	//button5.setBackground(new Color(120,120,120));
+            }
+        });
+        
+        JPanel button6 = new JPanel();
+        button6.setSize(120, 120);
+        button6.setBorder(null);
+        button6.setBackground(new Color(160,160,160));
+        button6.setLocation(720,300);  
+        button6.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button6.setBackground(new Color(160,160,160));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // Not working :(
+                button6.setBackground(new Color(200,200,200));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button6.setBorder(null);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button6.setBorder(mouseOverBorder);
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            	//button6.setBackground(new Color(120,120,120));
+            }
+        });
+        
         JProgressBar testPB = new JProgressBar();
         testPB.setBackground(Color.black);
         testPB.setForeground(new Color(160,160,160));
         testPB.setBorder(null);
         testPB.setBorderPainted(false);
-        testPB.setSize(100, 30);
-        testPB.setLocation(100, 100);
+        testPB.setSize(260, 40);
+        testPB.setLocation(580, 440);
         testPB.setValue(50);
         
         JPanel audioPanel = new JPanel();
@@ -91,6 +255,11 @@ public class GUI extends JFrame {
         
         add(testPB);
         add(button1);
+        add(button2);
+        add(button3);
+        add(button4);
+        add(button5);
+        add(button6);
         add(audioPanel);
         
     }
